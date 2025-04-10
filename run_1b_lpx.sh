0df59342f400ef2517495fed5d40e60d6c319baa
pixeli99 <pixeli@qq.com>
2025-04-10 17:30:10 +0800

    123

--- run_1b_lpx.sh
+++ run_1b_lpx.sh
@@ -14,7 +14,7 @@ echo "Training with learning rate: $learning_rates, norm type: $norm_type on GPU
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node 4 --master_port=29501 torchrun_main.py \
     --model_config configs/llama_1b.json \
     --lr $learning_rates \
-    --batch_size 128 \
+    --batch_size 64 \
     --total_batch_size 512 \
     --num_training_steps 100000 \
     --warmup_steps 1000 \

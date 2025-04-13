1cf1cce5786737ce21ddb14e8a0e3cc71d48e254
pixeli99 <pixeli@qq.com>
2025-04-13 14:52:17 +0800

    123

--- run_350m_pre_loop.sh
+++ run_350m_pre_loop.sh
@@ -12,10 +12,10 @@ export HF_HOME="/lpai/volumes/ad-vla-vol-ga/lipengxiang/vla/hf_cache"
 
 echo "Training with learning rate: $learning_rates, norm type: $norm_type on GPU $gpu"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node 4 --master_port=29511 torchrun_main.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node 4 --master_port=29501 torchrun_main.py \
     --model_config configs/llama_350m.json \
     --lr $learning_rates \
-    --batch_size 128 \
+    --batch_size 32 \
     --total_batch_size 512 \
     --num_training_steps 60000 \
     --warmup_steps 6000 \
@@ -24,7 +24,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node 4 --master_port=29511 tor
     --eval_every 1000 \
     --optimizer adam \
     --grad_clipping 0.0 \
-    --run_name "350m_res_${norm_type}_lr${learning_rates}_loop" \
+    --run_name "350m_res_${norm_type}_lr${learning_rates}_loop_linear" \
     --save_dir "${LPAI_MODEL_DIR}/350m_res_${norm_type}_lr${learning_rates}_loop" \
     --loop_enabled \
     --loop_schedule linear

13a93aede69f8405bb66541880b3e25437815ff8
pixeli99 <pixeli@qq.com>
2025-04-27 16:03:19 +0800

    linear gate

--- peft_pretraining/modeling_llama.py
+++ peft_pretraining/modeling_llama.py
@@ -318,7 +318,8 @@ class LlamaDecoderLayer(nn.Module):
         if norm_type == 'attn_skip':
             self.input_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
             self.post_attention_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
-            self.attn_sk = nn.Parameter(torch.ones(1, device='cuda'), requires_grad=True)
+            # self.attn_sk = nn.Parameter(torch.ones(1, device='cuda'), requires_grad=True)
+            self.attn_sk =  nn.Linear(config.hidden_size, config.hidden_size, bias=False)
         if norm_type == 'radia':
             self.input_layernorm = RadialNorm(config.hidden_size,)
             self.post_attention_layernorm = RadialNorm(config.hidden_size,)
@@ -491,7 +492,7 @@ class LlamaDecoderLayer(nn.Module):
                 output_attentions=output_attentions,
                 use_cache=use_cache,
             )
-            hidden_states = residual + hidden_states * self.attn_sk
+            hidden_states = residual + hidden_states * self.attn_sk(residual)
 
             residual = hidden_states
             hidden_states = self.post_attention_layernorm(hidden_states)
